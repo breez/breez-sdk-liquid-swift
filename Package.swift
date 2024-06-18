16a5f44c2efd84859d3f84a0230b959ea6abdce9
Roei Erez <roeierez@gmail.com>
2024-06-18 15:44:02 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .library(name: "BreezLiquidSDK", targets: ["breez_liquid_sdkFFI", "BreezLiquidSDK"]),
     ],
     targets: [
-        .binaryTarget(name: "breez_liquid_sdkFFI", url: "https://github.com/breez/breez-liquid-sdk-langs/swift/releases/download/0.1.0-rc1/breez_liquid_sdkFFI.xcframework.zip", checksum: "1ef9a621be477446c5e6780b996a393b5735414f594184838dbcdb0c3a9481ed"),
+        .binaryTarget(name: "breez_liquid_sdkFFI", url: "https://github.com/breez/breez-liquid-sdk-swift/releases/download/0.1.0-rc1/breez_liquid_sdkFFI.xcframework.zip", checksum: "1ef9a621be477446c5e6780b996a393b5735414f594184838dbcdb0c3a9481ed"),
         .target(name: "BreezLiquidSDK", dependencies: ["breez_liquid_sdkFFI"]),
     ]
 )
