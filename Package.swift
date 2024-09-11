66680e722cc4a31913e7c877b1ff0205b8e7b7a1
SDK release tagger <no-reply@breez.technology>
2024-09-11 14:35:27 +0000

    Update Swift bindings to version 0.3.1-dev3

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .library(name: "BreezSDKLiquid", targets: ["breez_sdk_liquidFFI", "BreezSDKLiquid"]),
     ],
     targets: [
-        .binaryTarget(name: "breez_sdk_liquidFFI", url: "https://github.com/breez/breez-sdk-liquid-swift/releases/download/0.3.1-dev2/breez_sdk_liquidFFI.xcframework.zip", checksum: "ea89bbc36ccea8023d5f037302bc0a42ceac4198076be40ce34d5c1614e277d3"),
+        .binaryTarget(name: "breez_sdk_liquidFFI", url: "https://github.com/breez/breez-sdk-liquid-swift/releases/download/0.3.1-dev3/breez_sdk_liquidFFI.xcframework.zip", checksum: "dc3cea0a84c06ea4df284d06af9c46ffcb713ddd8fc1ab80ab9eee60a177b62f"),
         .target(name: "BreezSDKLiquid", dependencies: ["breez_sdk_liquidFFI"]),
     ]
 )
