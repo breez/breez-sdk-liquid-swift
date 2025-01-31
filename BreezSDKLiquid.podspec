7d33d5714fc18cbe886e04efc1965ac34f222dc1
SDK release tagger <no-reply@breez.technology>
2025-01-31 11:11:38 +0000

    Update Swift bindings to version 0.6.4

--- BreezSDKLiquid.podspec
+++ BreezSDKLiquid.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |spec|
   spec.name                   = "BreezSDKLiquid"
-  spec.version                = "0.6.3"
+  spec.version                = "0.6.4"
   spec.license                = { :type => "MIT" }
   spec.summary                = "Swift bindings to the Breez Liquid SDK"
   spec.homepage               = "https://breez.technology"
--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .library(name: "BreezSDKLiquid", targets: ["breez_sdk_liquidFFI", "BreezSDKLiquid"]),
     ],
     targets: [
-        .binaryTarget(name: "breez_sdk_liquidFFI", url: "https://github.com/breez/breez-sdk-liquid-swift/releases/download/0.6.3/breez_sdk_liquidFFI.xcframework.zip", checksum: "90ce9d3e3c3dbafd4f2e81bc56d7ebffa38309765b0acf0bf1b698bbe256e3ec"),
+        .binaryTarget(name: "breez_sdk_liquidFFI", url: "https://github.com/breez/breez-sdk-liquid-swift/releases/download/0.6.4/breez_sdk_liquidFFI.xcframework.zip", checksum: "056b89ff22f6104b847a30a5a27851a7dcfcdcd236ade6d3904cf3c50da6ef54"),
         .target(name: "BreezSDKLiquid", dependencies: ["breez_sdk_liquidFFI"]),
     ]
 )
